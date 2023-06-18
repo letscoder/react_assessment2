eacebb306cd6394408c6106f47126c5368ecbe2e
unknown <ankeerthi2002@gmail.com>
2023-06-18 15:47:36 +0530

    first commit

---
+++ src/welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let WelcomePage
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>Good morning .How are you doing today?</p>
+      
+    </div>
+  );
+
+
+export default Welcome;
